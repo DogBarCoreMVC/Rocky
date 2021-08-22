27a503ba6d69bc8712c47084fe7c2f4796666126
GitDogBar <Dogbar>
2021-08-23 00:33:57 +0700

    Connection Database / Create Table in Index.cshtml in CategoryController.cs

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,17 @@ namespace Rocky.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;//Dependency Injection / readonly เป็น properties
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;//Object db รับค่าข้อมูลมาจาก Class ApplicationDbContext ที่อยู่ใน Method ConfiguretionServices แล้วนำข้อมูลมาเก็บไว้ใน _db
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> ObjList = _db.CategoryTbl;//เอาข้อมูลที่ได้จาก CategoryTbl ไปเก็บไว้ที่ ObjList
+            return View(ObjList);//แสดงข้อมูลใน ObjList
         }
     }
 }
