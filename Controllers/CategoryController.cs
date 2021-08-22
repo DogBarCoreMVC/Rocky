8650a3180d68609cc1a02bc8291712ce8e85a42e
GitDogBar <Dogbar>
2021-08-23 02:23:45 +0700

    POST

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -17,10 +17,27 @@ namespace Rocky.Controllers
             _db = db;//Object db รับค่าข้อมูลมาจาก Class ApplicationDbContext ที่อยู่ใน Method ConfiguretionServices แล้วนำข้อมูลมาเก็บไว้ใน _db
         }
 
-        public IActionResult Index()
+        
+        public IActionResult Index()//แสดงข้อมูลที่มีอยู่ใน DataBase
         {
             IEnumerable<Category> ObjList = _db.CategoryTbl;//เอาข้อมูลที่ได้จาก CategoryTbl ไปเก็บไว้ที่ ObjList
             return View(ObjList);//แสดงข้อมูลใน ObjList
+        }
+
+        //GET - 
+        public IActionResult Create()//ใส่ข้อมูลลง DataBase
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost] //กำหนด attribute เพื่อใช้ในการรับข้อมูล
+        [ValidateAntiForgeryToken] //ความปลอดภัย
+        public IActionResult Create(Category Obj)
+        { 
+            _db.CategoryTbl.Add(Obj);//รับข้อมูลจาก Object ด้วย Method Add
+            _db.SaveChanges();//บันทึกลง DataBase
+            return View();
         }
     }
 }
